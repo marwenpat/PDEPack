2802cf04e3591152f10a73e521bb5d6acc2ea44b
Manfred Brill <MBrill@users.noreply.github.com>
2017-06-02 16:07:10 +0200

    Add files via upload

---
+++ examples/TriLU/luTest.cpp
@@ -0,0 +1,54 @@
+// ---------------------------------------------------------------------------
+// Einlesen und Lösen eines tridiagonalen LGS
+// ---------------------------------------------------------------------------
+#include <cmath>
+#include <iostream>
+
+#include <tnt.h>
+#include "utils.h"
+
+#include "TriDiag.h"
+#include "TriLU.h"
+
+using namespace std;
+
+/*!
+ * \example LU-Zerlegung und Lösung
+ *
+ * LU-Zerlegung für tridiagonale Matrizen. 
+ * 
+ * Wir instanziieren eine tridiagonale Matrix mit konstanten
+ * Elementen in den Diagonalen, führen eine LU-Zerlegung
+ * durch und verwenden den ersten Einheitsvektor als rechte Seite.
+ *
+ * Wir evaluieren die Lösung, indem wir sie mit der Matrix multiplizieren
+ * und mit dem ersten Einheitsvektor vergleichen. Die Abweichung sollte
+ * gering sein!
+ */
+int main(void)
+{
+	int n(5);
+
+	TriDiag tri(n, 2.0, -1.0, -1.0);
+
+	// LU instanziieren und Lösung ausgeben
+	TriLU lu(tri);
+
+	// LU-Zerlegung ausgeben
+	lu.print();
+
+	// Rechte Seite
+	TNT::Vector<double> rhs = unitVector(0, n);
+	TNT::Vector<double> x = lu.solve(rhs);
+
+	std::cout << "Die berechnete Loesung" << std::endl;
+	std::cout << x;
+
+	// Probe
+	TNT::Vector<double> ax(tri.ATimesX(x));
+	std::cout << "Wir berechnen A*x:" << std::endl;
+	std::cout << ax;
+
+	std::cout << std::scientific << "Die Abweichung zwischen der rechten Seite und A*X: " << normMaximum(ax - rhs) << std::endl;
+    return 0;
+}
---
+++ examples/TriLU/triTest.cpp
@@ -0,0 +1,44 @@
+// ---------------------------------------------------------------------------
+// Einlesen und Lösen eines tridiagonalen LGS
+// ---------------------------------------------------------------------------
+#include <cmath>
+#include <iostream>
+
+#include <tnt.h>
+
+#include "TriDiag.h"
+
+using namespace std;
+
+/*!
+ * \example TriDiag
+ *
+ * Wir testen die Variante, die drei Vektoren für die Speicherung
+ * verwendet.
+*/
+int main(void)
+{
+	int n(5);
+
+	TriDiag tri(n, 2.0, -1.0, -1.0);
+
+	tri.print();
+
+	// ATimesX testen
+	TNT::Vector<double> x(n, 1.0);
+	std::cout << "Test der Multiplikation mit einem Vektor" << std::endl;
+	std::cout << "Das Ergebnis muss der Vektor (1,0,0,0,1) sein!" << std::endl;
+	std::cout << tri.ATimesX(x);
+
+	TNT::Vector<double> a(n, 4.0), b(n - 1, -1.0), c(n - 1, -1.0);
+	TriDiag tri2(a, b, c);
+
+	tri2.print();
+
+	// ATimesX testen
+	std::cout << "Test der Multiplikation mit einem Vektor" << std::endl;
+	std::cout << "Das Ergebnis muss der Vektor (3, 2, 2, 2,  3) sein!" << std::endl;
+	std::cout << tri2.ATimesX(x);
+
+    return 0;
+}
